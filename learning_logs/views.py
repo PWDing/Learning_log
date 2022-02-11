ae1723908f54895ab80de28803a5e471b4a666da
pluto <xpwding@gmail.com>
2022-02-11 16:24:18 +0800

    permit user add topic and entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,11 @@
+from django.forms import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.template import context
+from django.urls import reverse
+
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -19,5 +25,33 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
-def entries(request):
-    """展示所有的文章"""
+def new_topic(request):
+    """添加一个新主题"""
+    if request.method != 'POST':
+        # 没有数据被提交，创建空表单
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """添加一篇新文章"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
