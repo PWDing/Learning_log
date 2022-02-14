0a9b3e2d21ab45f39d376eeaed2b962e7ce8cf14
pluto <xpwding@gmail.com>
2022-02-14 19:13:06 +0800

    style pages using bootstrap

--- learning/settings.py
+++ learning/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     # My apps
     'learning_logs',
     'users',
+
+    # Third party apps
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # My settings
 LOGIN_URL = '/users/login/'
+
+# Settings for django-bootstrap3
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
