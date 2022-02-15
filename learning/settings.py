3ba6b536ca296a056916f9afdb4b0a3011b95c6a
pluto <xpwding@gmail.com>
2022-02-15 15:30:52 +0800

    close debug

--- learning/settings.py
+++ learning/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-==75ebquahrhb74%%)16ie3vr68y3ii7#21hljz7%pshzeovav'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
